e6ecf5ddfc45e23a1b0f647d385fe86fedb0fa57
VladyslavBohutskyi <nonseinvenit@gmail.com>
2024-04-10 16:00:33 +0800

    clean project

---
+++ dev/src/global.js
@@ -0,0 +1,16 @@
+// vendor
+window.Flickity = require('flickity');
+require('lazysizes');
+
+// modules
+window.PubSub = require('./scripts/pubsub');
+
+function debounce(fn, wait) {
+	let t;
+	return (...args) => {
+		clearTimeout(t);
+		t = setTimeout(() => fn.apply(this, args), wait);
+	};
+}
+
+
