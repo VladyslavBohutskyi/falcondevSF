e5940b12893fddb6744bbb9e4b923ad653629cca
VladyslavBohutskyi <nonseinvenit@gmail.com>
2024-04-18 15:52:56 +0800

    header css progress

---
+++ dev/src/s-header.js
@@ -0,0 +1,10 @@
+if (!customElements.get('s-header')) {
+
+    customElements.define('s-header', class SHeader extends HTMLElement {
+        constructor() {
+            super();
+        }
+
+        
+    })
+}
