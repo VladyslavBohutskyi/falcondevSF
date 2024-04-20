f05dcd437d5004c85958d1fe7fb4c0a8ae68d53d
VladyslavBohutskyi <nonseinvenit@gmail.com>
2024-04-20 14:08:36 +0800

    add burger

--- dev/src/s-header.js
+++ dev/src/s-header.js
@@ -1,10 +1,21 @@
 if (!customElements.get('s-header')) {
-
     customElements.define('s-header', class SHeader extends HTMLElement {
-        constructor() {
-            super();
-        }
+      constructor() {
+        super();
+      }
+      
+      connectedCallback() {
+        console.log('ggg');
+        this.openMobileMenu()
+      }
 
-        
+      openMobileMenu() {
+        console.log('func');
+        this.querySelector('.mobile-menu__btn').addEventListener('click', () => {
+            this.querySelector('.mobile-menu__btn').classList.toggle('mobile-menu__btn--active')
+        }
+        )
+    }
+
     })
-}
+  }
