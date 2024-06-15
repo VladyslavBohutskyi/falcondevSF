99c15757f76c424a4461a080c06e6bbaf3a66904
VladyslavBohutskyi <nonseinvenit@gmail.com>
2024-06-15 12:17:05 +0800

    main-collection

---
+++ dev/src/main-collection.js
@@ -0,0 +1,12 @@
+if (!customElements.get('main-collection')) {
+    customElements.define('main-collection', class MainCollection extends HTMLElement {
+      constructor() {
+        super();
+      }
+      
+
+
+      
+    //   searchParams = new URLSearchParams(window.location.search)
+    })
+  }
